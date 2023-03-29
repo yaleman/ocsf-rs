c1f527b2938627fa36ca90ed686393ba703acf69
James Hodgkinson <james@terminaloutcomes.com>
2023-03-29 21:21:42 +1000

    actually adding the example code...

---
+++ examples/examples/test.rs
@@ -0,0 +1,8 @@
+use ocsf::events::BaseEvent;
+
+fn main() {
+    let base_event = BaseEvent::default();
+    println!("BaseEvent::default() prettyprint:\n{:#?}", base_event);
+
+    println!("BaseEvent::default() json:\n{}", serde_json::to_string(&base_event).unwrap());
+}
